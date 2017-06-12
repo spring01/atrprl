a70941a7bebc865b5495b8596e40ffa48f96a301
spring01 <lihc2012@gmail.com>
2017-06-12 17:02:15 -0400

    fix test.py

--- test.py
+++ test.py
@@ -5,9 +5,9 @@ os.environ['OMP_NUM_THREADS'] = '1'
 
 from atrp_env import ATRPEnv
 
-env = ATRPEnv(timestep=1e6)
+env = ATRPEnv()
 state = env.reset()
-state, reward, done, info = env.step(0)
+state, reward, done, info = env.step(env.action_space.sample())
 env.render()
 print state
 
