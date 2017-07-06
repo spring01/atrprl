bf7397e1a6ba2ad0514b36b4e909cd4435edeb7d
spring01 <lihc2012@gmail.com>
2017-07-05 21:03:37 -0400

    register ATRP-polystyrene-v0 on importing

--- atrp_ps.py
+++ atrp_ps.py
@@ -1,8 +1,4 @@
 
-import os
-os.environ['OMP_NUM_THREADS'] = '1'
-
-import gym
 from atrp import ATRPEnv
 from gym.envs.registration import register
 
@@ -40,25 +36,3 @@ register(
     }
 )
 
-
-
-env = gym.make('ATRP-polystyrene-v0')
-
-
-
-
-state = env.reset()
-#~ env.render()
-total_reward = 0.0
-for i in range(3000):
-    state, reward, done, info = env.step(env.action_space.sample())
-    total_reward += reward
-    env.render()
-    volume = '{:2.2f}'.format(env.unwrapped.volume)
-    added = env.unwrapped.added
-    print(i, reward, volume, round(added[0], 2), round(added[1], 2),
-          round(added[2], 2), round(added[3], 2), round(added[4], 2))
-    if done:
-        break
-#~ env.render()
-print('total reward:', total_reward)
