7bddbb0bd4492a3a0b0c4c836c6b1f0290e93947
spring01 <lihc2012@gmail.com>
2017-07-24 18:53:25 -0400

    rename env into ATRP-ps-td-simple-v0

--- atrp_ps_td.py
+++ atrp_ps_td.py
@@ -38,43 +38,7 @@ simple = [2.33490714e-10,   4.99837917e-09,   5.35990968e-08,
           7.08342143e-06]
 
 register(
-    id='ATRP-ps-td-v1',
-    entry_point='atrp:ATRPTargetDistrib',
-    max_episode_steps=100000,
-    kwargs={
-        'max_rad_len': 100,
-        'step_time': 1e2,
-        'completion_time': 1e5,
-        'min_steps': 100,
-        'termination': False,
-        'k_prop': 1.6e3,
-        'k_act': 0.45,
-        'k_deact': 1.1e7,
-        'k_ter': 1e8,
-        'observation_mode': 'all',
-        'mono_init': 0.0,
-        'cu1_init': 0.0,
-        'cu2_init': 0.0,
-        'dorm1_init': 0.0,
-        'mono_unit': 0.1,
-        'cu1_unit': 0.004,
-        'cu2_unit': 0.004,
-        'dorm1_unit': 0.008,
-        'mono_cap': 10.0,
-        'cu1_cap': 0.2,
-        'cu2_cap': 0.2,
-        'dorm1_cap': 0.4,
-        'mono_density': 8.73,
-        'sol_init': 0.01,
-        'sol_cap': 0.0,
-        'reward_chain_type': 'dorm',
-        'dn_distribution': simple,
-        'ks_num_sample': 2e3,
-    }
-)
-
-register(
-    id='ATRP-ps-td-v2',
+    id='ATRP-ps-td-simple-v0',
     entry_point='atrp:ATRPTargetDistrib',
     max_episode_steps=100000,
     kwargs={
